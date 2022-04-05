81ac23fa51534bb2ee529dbc681215202ba61d41
Yogi Delfiandra <yogi.delfiandra03@gmail.com>
2022-04-05 11:02:39 +0700

    repair code scrollToTop button

--- src/components/scroll/ScrollToTop.js
+++ src/components/scroll/ScrollToTop.js
@@ -36,10 +36,10 @@ export const ScrollToTop = () => {
         onClick={scrollToTop}
         className={classNames(
           isVisible ? 'opacity-100' : 'opacity-0',
-          'inline-flex items-center p-3 rounded-full shadow-sm text-white bg-primary transition-opacity hover:bg-opacity-80 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary'
+          'inline-flex items-center p-3 rounded-full shadow-sm text-white bg-secondary bg-opacity-20 transition duration-500 hover:bg-primary hover:bg-opacity-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary'
         )}
       >
-        <GoArrowUp className='h-6 w-6' aria-hidden='true' />
+        <GoArrowUp className='h-6 w-6 opacity-80' aria-hidden='true' />
       </button>
     </div>
   );
