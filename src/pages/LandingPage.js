e878284206ff5ad06c02139200b52381f24cc247
Yogi Delfiandra <yogi.delfiandra03@gmail.com>
2022-04-28 16:00:57 +0700

    clean code part Experience

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -3,14 +3,11 @@ import { ScrollToTop } from '../components/scroll';
 import useScrollAnchor from '../helpers/hooks/useScrollAnchor';
 import landingPage from '../json/landingPage.json';
 import About from '../parts/About';
-// import Blog from '../parts/Blog';
-// import Clients from '../parts/Clients';
-// import Contact from '../parts/Contact';
-// import Experience from '../parts/Experience';
+// import Portfolio from '../parts/Portfolio';
+import Experience from '../parts/Experience';
 // import Footer from '../parts/Footer';
 import Header from '../parts/Header';
 import Hero from '../parts/Hero';
-// import Portfolio from '../parts/Portfolio';
 
 export default function LandingPage() {
   useScrollAnchor();
@@ -21,7 +18,7 @@ export default function LandingPage() {
       <Hero />
       <main className='dark:bg-black'>
         <About data={landingPage.contacts} />
-        {/* <Experience data={landingPage.skills} /> */}
+        <Experience data={landingPage.skills} />
         {/* <Portfolio data={landingPage.portfolios} /> */}
         {/* <Clients data={landingPage.clients} /> */}
         {/* <Blog data={landingPage.blogs} /> */}
--- src/parts/Experience.js
+++ src/parts/Experience.js
@@ -3,20 +3,14 @@ import { BsPatchCheckFill } from 'react-icons/bs';
 
 export default function Experience(props) {
   return (
-    <section
-      id='experience'
-      className='bg-slate-100 pt-36 pb-32 dark:bg-slate-700'
-    >
+    <section id='experience' className='experience-section'>
       <div className='container'>
-        <div className='w-full px-4'>
+        <div className='w-full'>
           <div className='mx-auto mb-16 max-w-xl text-center'>
-            <h3 className='mb-2 text-lg font-semibold text-primary'>Skills</h3>
-            <h4 className='mb-4 text-3xl font-bold text-dark dark:text-white sm:text-4xl lg:text-5xl'>
-              Keterampilan Teknis
-            </h4>
-            <p className='text- text-base font-medium text-secondary md:text-lg'>
-              Kumpulan teknologi yang saya gunakan <br className='' /> untuk
-              membangun sebuah aplikasi web
+            <h3 className='sub-title mb-2'>Skills</h3>
+            <h4 className='title lg:text-4xl'>My Experience</h4>
+            <p className='paragraph'>
+              Some of the tools I use to <br /> build web applications
             </p>
           </div>
         </div>
@@ -24,21 +18,18 @@ export default function Experience(props) {
         <div className='flex flex-wrap justify-center'>
           {props.data.map((item, index1) => {
             return (
-              <div
-                key={index1}
-                className='mb-12 w-full px-4 sm:w-[500px] md:w-[600px] lg:mb-0 lg:w-1/2 lg:px-4'
-              >
-                <div className='rounded-xl bg-slate-800 py-4 text-center text-white shadow-sm transition duration-500 hover:bg-slate-700 dark:bg-dark dark:hover:bg-slate-800'>
-                  <h5 className='mb-5 py-6 text-lg font-bold xl:mb-10  xl:text-xl'>
+              <div key={index1} className='column-3'>
+                <div className='card'>
+                  <h5 className='mb-5 py-6 text-lg font-bold xl:mb-10 xl:text-xl'>
                     {item.title}
                   </h5>
 
-                  <div className='flex w-full flex-wrap sm:px-10 md:px-10'>
+                  <div className='card-item'>
                     {item.features.map((feature, index2) => {
                       return (
                         <div
                           key={`skills-${index1}-feature-${index2}`}
-                          className='mb-12 flex w-1/2 items-center px-6 md:mb-10 md:px-16 lg:px-10 xl:px-16 '
+                          className='skill-item'
                         >
                           <BsPatchCheckFill
                             size={16}
@@ -46,10 +37,10 @@ export default function Experience(props) {
                             style={{ marginTop: 3 }}
                           />
                           <div className='pl-4 text-left'>
-                            <h6 className='text-sm font-bold md:text-base xl:text-[17px]'>
+                            <h6 className='text-sm font-semibold md:text-base xl:text-[17px]'>
                               {feature.name}
                             </h6>
-                            <p className='text-xs md:text-sm xl:text-[15px]'>
+                            <p className='text-xs font-normal md:text-sm xl:text-[15px]'>
                               {feature.level}
                             </p>
                           </div>
